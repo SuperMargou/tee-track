810185e738588c230f51dfae06180ee45a16ce43
SuperMargou <armand.goutpayet@gmail.com>
2025-05-24 11:24:17 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,11 @@ import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(reg => console.log('Service Worker registered:', reg))
+      .catch(err => console.error('Service Worker registration failed:', err));
+  });
+}
